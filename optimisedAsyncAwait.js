f3e43858e02a5a69ea5e1d5e7e4d1259dbd59ba9
Samuel Thng <samuel@thng.sg>
2020-10-17 03:46:49 +0800

    Minor Cleanup.

--- optimisedAsyncAwait.js
+++ optimisedAsyncAwait.js
@@ -1,7 +1,5 @@
 require('colors');
 const prompt = require('prompt-sync')({sigint: true});
-const adamjsname = `ADAM.js`
-const adamjsquote = `[ADAM.js]`
 const performTimeout = (ms) => new Promise(resolve => setTimeout(resolve, ms));
 const runTaskList = async tasklist => {
 	for (let { text, fnToRun, timeout } of tasklist) {
@@ -13,13 +11,13 @@ const runTaskList = async tasklist => {
 const checkForAdam = async () => {
 	const username = prompt('');
 	await performTimeout(1200); // Not sure why wait for 1.2 secs but ok.
-	await runTaskList(username === 'Adam' 
+	await runTaskList(username.toLocaleLowerCase() === 'adam' 
 	? [
 		{ text: `\n(Unsurprisingly, You found the special name.)\n`.yellow, timeout: 4000 },
-		{ text: adamjsquote.green + ` Oh..`, timeout: 1000 },
-		{ text: adamjsquote.green + ` Ummm..`, timeout: 3000 },
-		{ text: adamjsquote.green + ` W-Well, hello, Me!\n`, timeout: 3000 },
-	] : [{ text: adamjsquote.green +` Well, hello, ${username}!\n`, timeout: 3000 }]);
+		{ text: `${'[ADAM.js]'.green} Oh..`, timeout: 1000 },
+		{ text: `${'[ADAM.js]'.green} Ummm..`, timeout: 3000 },
+		{ text: `${'[ADAM.js]'.green} W-Well, hello, Me!\n`, timeout: 3000 },
+	] : [{ text: `${'[ADAM.js]'.green} Well, hello, ${username}!\n`, timeout: 3000 }]);
 }
 const thingsToDo = [
 	{ fnToRun: console.clear, timeout: 1000 },
@@ -29,12 +27,12 @@ const thingsToDo = [
 	{ text: ` AA     AA   DD   DD   AA     AA   MM   MMMM   MM`.rainbow.bold, timeout: 500 },
 	{ text: ` AA     AA   DDDDDD    AA     AA   MM    MM    MM ● js\n`.rainbow.bold, timeout: 1000 },
 	{ timeout: 1000 },
-	{ text: `${adamjsname.green.bold}, just a friendly file.`, timeout: 1000 },
+	{ text: `${'ADAM.js'.green.bold}, just a friendly file.`, timeout: 1000 },
 	{ text: `Made with ${`love`.red.bold} by Adam.\n`, timeout: 2000 },
-	{ text: adamjsquote.green + ` Welcome to the ` + adamjsname.green.bold + ` program! What is your name?` + ` (Hint: There's also a very special name you can try..)\n`.yellow.bold },
+	{ text: `${'[ADAM.js]'.green} Welcome to the ${'ADAM.js'.green.bold} program! What is your name? ${"(Hint: There's also a very special name you can try..)\n".yellow.bold}`},
 	{ fnToRun: checkForAdam },
-	{ text: adamjsquote.green + ` So, how is your fine day going?`, timeout: 2000 },
-	{ text: adamjsquote.green + ` Good? Bad? Well, either way, here's something YOU should know.`, timeout: 4000 },
-	{ text: adamjsquote.green + ` You certainly made ${`MY`.yellow.bold} day, by running me!\n` },
+	{ text: `${'[ADAM.js]'.green} So, how is your fine day going?`, timeout: 2000 },
+	{ text: `${'[ADAM.js]'.green} Good? Bad? Well, either way, here's something YOU should know.`, timeout: 4000 },
+	{ text: `${'[ADAM.js]'.green} You certainly made ${`MY`.yellow.bold} day, by running me!\n` },
 ];
 runTaskList(thingsToDo);
